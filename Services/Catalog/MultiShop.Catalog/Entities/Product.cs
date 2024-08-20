ee1ca4ef35b2e19cebcafbac30582e23576c28e7
Fethi Güralp Albayram <fethi.grlp@gmail.com>
2024-08-20 11:22:47 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string? productName { get; set; }
+        public decimal productPrice { get; set; }
+        public int productStock { get; set; }
+        public string? productImageUrl { get; set; }
+        public string? productDescription { get; set; }
+        public string? categoryId { get; set; }
+
+        // [BsonIgnore] tells MongoDB not to keep this value in the database
+
+        [BsonIgnore] 
+        public Category? category { get; set; }
+    }
+}
